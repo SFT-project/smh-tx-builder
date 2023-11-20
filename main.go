967921ad054a3ad367eca67c1957f7359794a971
gpt-dev <litekok@gmail.com>
2023-11-20 09:52:41 +0800

    error message fixed.

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func main() {
 func sendMoney(privatekey string, address string, amounts []uint64, addressList []string) {
 	ctx := context.Background()
 	if len(amounts) != len(addressList) {
-		panic("发放的收益和地址要保持一样")
+		panic("the size of address should be equal with the length of amount array.")
 	}
 	cc, _ := grpc.Dial("localhost:9092", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	defer cc.Close()
@@ -89,42 +89,3 @@ func sendMoney(privatekey string, address string, amounts []uint64, addressList 
 	}
 
 }
-
-//func origin()  {
-//	recipientAddress, _ := types.StringToAddress(recipientAddressStr)
-//	cc, _ := grpc.Dial(nodeUri, grpc.WithTransportCredentials(insecure.NewCredentials()))
-//	defer cc.Close()
-//
-//	meshClient := api.NewMeshServiceClient(cc)
-//	meshResp, _ := meshClient.GenesisID(ctx, &api.GenesisIDRequest{})
-//	var genesisId types.Hash20
-//	copy(genesisId[:], meshResp.GenesisId)
-//
-//	client := api.NewNodeServiceClient(cc)
-//	statusResp, _ := client.Status(ctx, &api.StatusRequest{})
-//
-//	gstate := api.NewGlobalStateServiceClient(cc)
-//	resp, _ := gstate.Account(ctx, &api.AccountRequest{AccountId: &api.AccountId{Address: principal.String()}})
-//	nonce := resp.AccountWrapper.StateProjected.Counter
-//	balance := resp.AccountWrapper.StateProjected.Balance
-//	fmt.Printf("Sender nonce %d balance %d\n", nonce, balance.Value)
-//
-//	// generate the tx
-//	tx := walletSdk.Spend(signing.PrivateKey(privkey), recipientAddress, amount, nonce+1,
-//		sdk.WithGenesisID(genesisId),
-//	)
-//	fmt.Printf("Generated signed tx: %s\n", hex.EncodeToString(tx))
-//
-//	// parse it
-//	txService := api.NewTransactionServiceClient(cc)
-//	txResp, _ := txService.ParseTransaction(ctx, &api.ParseTransactionRequest{Transaction: tx})
-//	fmt.Printf("parsed tx: principal: %s, gasprice: %d, maxgas: %d, nonce: %d\n",
-//		txResp.Tx.Principal.Address, txResp.Tx.GasPrice, txResp.Tx.MaxGas, txResp.Tx.Nonce.Counter)
-//
-//	// broadcast it
-//	sendResp, _ := txService.SubmitTransaction(ctx, &api.SubmitTransactionRequest{Transaction: tx})
-//
-//	// return the txid
-//	fmt.Printf("status code: %d, txid: %s, tx state: %s\n",
-//		sendResp.Status.Code, hex.EncodeToString(sendResp.Txstate.Id.Id), sendResp.Txstate.State.String())
-//}
